a681bd10d27f78e3615b9d32df8cbf770ac32cd6
jasleen0604 <66011969+jasleen0604@users.noreply.github.com>
2021-04-24 15:39:51 +0530

    initial commit made

---
+++ activity1/query.js
@@ -0,0 +1,11 @@
+let colorbtn = document.querySelector(".filter");
+let mainContainer = document.querySelector(".bottom-container");
+
+for(let i=0; i<colorbtn.length; i++)
+{
+    colorbtn[i].addEventListener("click", function(e){
+        let color=colorbtn[i].classList[0];
+
+        mainContainer.style.backgroundColor=color;
+    });
+}
